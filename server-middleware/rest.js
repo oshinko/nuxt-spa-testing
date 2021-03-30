9ef8d9ac5c7985045f4d122797a3e532092a3b77
Oshinko <osnk@renjaku.jp>
2021-03-30 21:51:45 +0900

    Add Swagger utility modules

--- server-middleware/rest.js
+++ server-middleware/rest.js
@@ -1,6 +1,33 @@
-const app = require('express')()
+import express from 'express'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 
-app.all('/headers', (req, res) => {
+const app = express()
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hello World',
+      version: '1.0.0',
+    },
+  },
+  apis: [__dirname + '/rest.js'],
+}
+
+swaggerJsdoc(options).then(doc =>
+  app.use('/spec', swaggerUi.serve, swaggerUi.setup(doc)))
+
+/**
+ * @openapi
+ * /headers:
+ *   get:
+ *     description: Get all HTTP headers
+ *     responses:
+ *       200:
+ *         description: Returns headers.
+ */
+app.get('/headers', (req, res) => {
   res.json(req.headers)
 })
 
